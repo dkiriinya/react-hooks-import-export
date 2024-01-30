7169600b28284ac6a2acc78ce7e601b86859c818
dkiriinya <dkiriinya06sc@gmail.com>
2024-01-30 15:07:38 +0300

    learnt different methods of importing and exporting

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,12 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees, wildlife } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  trees()
+  wildlife()
 
   return <h1>Colorado State Parks!</h1>;
+  
 }
